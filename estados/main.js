9fade0e3e76cea8bf510d58eeebebdc415a6b5da
carmoreno <carlosandresmorenovelez@gmail.com>
2017-01-07 18:39:13 -0500

    Tarjeta con información al lado derecho, imágenes editadas.

--- estados/main.js
+++ estados/main.js
@@ -1,4 +1,4 @@
-var juego = new Phaser.Game(774, 581, Phaser.AUTO, "div_juego")
+var juego = new Phaser.Game(774, 559, Phaser.AUTO, "div_juego")
 var modal = new gameModal(juego)// Objeto para la gestion de mensjaes (modales) en el juego
 juego.state.add('Boot', Boot)
 juego.state.add('Preloader', Preloader)
